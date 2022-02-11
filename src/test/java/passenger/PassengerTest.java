98b254ad52ba114a4d603f946937aaf5ea340ae9
garyb6 <gary.bennett6@outlook.com>
2022-02-11 20:04:42 +0000

    passenger TDD completed

--- src/test/java/passenger/PassengerTest.java
+++ src/test/java/passenger/PassengerTest.java
@@ -1,4 +1,26 @@
 package passenger;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
+
+    Passenger passenger;
+
+    @Before
+    public void before(){
+        passenger = new Passenger("Sawyer", 2);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Sawyer", passenger.getName());
+    }
+
+    @Test
+    public void hasBags(){
+        assertEquals(2, passenger.getBags());
+    }
 }
