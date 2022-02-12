74da42551d099f7afa92aae0883e5f8007eb3009
garyb6 <gary.bennett6@outlook.com>
2022-02-12 13:27:20 +0000

    pilot extended from cabincrewmember and getters inherited and licenceNum passed

--- src/main/java/staff/Pilot.java
+++ src/main/java/staff/Pilot.java
@@ -1,4 +1,16 @@
 package staff;
 
-public class Pilot {
+public class Pilot extends CabinCrewMember {
+
+    private String licenceNum;
+
+    public Pilot(String name, RankType rank, String licenceNum) {
+        super(name, rank);
+        this.licenceNum = licenceNum;
+    }
+
+
+    public String getLicenceNum() {
+        return this.licenceNum;
+    }
 }
--- src/test/java/staff/PilotTest.java
+++ src/test/java/staff/PilotTest.java
@@ -1,4 +1,30 @@
 package staff;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class PilotTest {
+
+    Pilot pilot;
+
+    @Before
+    public void before(){
+        pilot = new Pilot ("Desmond", RankType.Captain, "li123");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Desmond", pilot.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(RankType.Captain, pilot.getRank());
+    }
+
+    @Test
+    public void hasLicenceNum(){
+        assertEquals("li123", pilot.getLicenceNum());
+    }
 }
