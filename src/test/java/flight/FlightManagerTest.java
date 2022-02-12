0867f9ad3aa37092e9ec73e070604b1b15e2f0ee
garyb6 <gary.bennett6@outlook.com>
2022-02-12 18:35:11 +0000

    flightmanagertest constructor complete

--- src/test/java/flight/FlightManagerTest.java
+++ src/test/java/flight/FlightManagerTest.java
@@ -3,15 +3,26 @@ package flight;
 import org.junit.Before;
 import passenger.Passenger;
 import plane.Plane;
+import plane.PlaneType;
+import staff.Pilot;
+import staff.RankType;
 
 public class FlightManagerTest {
 
     Flight flight;
-    Passenger passenger;
+    Passenger passenger1;
+    Passenger passenger2;
+    Pilot pilot;
+    Plane plane;
 
     @Before
     public void before(){
-        passenger = newPass
+        passenger1 = new Passenger ("Locke", 2);
+        passenger2 = new Passenger ("Sayid", 4);
+        pilot = new Pilot ("Amelia Earhart", RankType.Captain, "LI078");
+        plane = new Plane (PlaneType.AIRBUSA330);
+        flight = new Flight (pilot, plane, "OA124", "BAR", "NEW", "18:30" );
     }
+
 
 }
