97dd986c2df94d21930227b2fc46f4653efa02da
garyb6 <gary.bennett6@outlook.com>
2022-02-12 13:51:35 +0000

    flight constructor established

--- src/main/java/flight/Flight.java
+++ src/main/java/flight/Flight.java
@@ -1,4 +1,49 @@
 package flight;
 
+import passenger.Passenger;
+import plane.Plane;
+import staff.CabinCrewMember;
+import staff.Pilot;
+import staff.RankType;
+
+import java.util.ArrayList;
+
 public class Flight {
+
+    Pilot pilot;
+    Plane plane;
+    ArrayList<Passenger> passengerManifesto;
+    ArrayList<CabinCrewMember> crewList;
+    String flightNum;
+    String destination;
+    String departureAirport;
+    String departureTime;
+
+
+    public Flight(Pilot pilot, Plane plane, String flightNum, String destination, String departureAirport, String departureTime  ) {
+        this.pilot = pilot;
+        this.crewList = new ArrayList<CabinCrewMember>();
+        this.passengerManifesto = new ArrayList<Passenger>();
+        this.plane = plane;
+        this.flightNum = flightNum;
+        this.destination = destination;
+        this.departureAirport = departureAirport;
+        this.departureTime = departureTime;
+    }
+
+    public String getFlightNum() {
+        return flightNum;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
 }
--- src/test/java/flight/FlightTest.java
+++ src/test/java/flight/FlightTest.java
@@ -1,4 +1,25 @@
 package flight;
 
+import org.junit.Before;
+import passenger.Passenger;
+import plane.Plane;
+import staff.CabinCrewMember;
+import staff.Pilot;
+
 public class FlightTest {
+
+    Flight flight;
+    Pilot pilot;
+    CabinCrewMember cabinCrewMember;
+    Passenger passenger;
+    Plane plane;
+
+
+
+
+
+    @Before
+    public void before(){
+        flight = new Flight();
+    }
 }
