3f96106f1463da1e1b81262f525bf76dc59a8761
garyb6 <gary.bennett6@outlook.com>
2022-02-12 13:18:43 +0000

    cabincrewmember class constructor build and getter tests passed

--- src/main/java/staff/CabinCrewMember.java
+++ src/main/java/staff/CabinCrewMember.java
@@ -1,4 +1,21 @@
 package staff;
 
 public class CabinCrewMember {
+
+    private String name;
+    private RankType rank;
+
+    public CabinCrewMember(String name, RankType rank) {
+        this.name = name;
+        this.rank = rank;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public RankType getRank() {
+        return this.rank;
+    }
+
 }
--- src/test/java/staff/CabinCrewMemberTest.java
+++ src/test/java/staff/CabinCrewMemberTest.java
@@ -1,4 +1,25 @@
 package staff;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class CabinCrewMemberTest {
+
+    CabinCrewMember cabincrewmember;
+
+    @Before
+    public void before(){
+        cabincrewmember = new CabinCrewMember("Richard", RankType.SeniorCrew);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Richard", cabincrewmember.getName());
+    }
+
+    @Test
+    public void hasRank(){
+        assertEquals(RankType.SeniorCrew, cabincrewmember.getRank());
+    }
 }
